38911c2f272efe0539c6c9d297752976bea6dfec
calexil <calexil@yahoo.com>
2016-08-23 21:49:52 -0400

    fixing precision aim 1.0

--- install.sh
+++ install.sh
@@ -3,6 +3,6 @@
 gksudo mkdir /etc/X11/xorg.conf.d/ 
 sudo touch /etc/X11/xorg.conf.d/910-rat.conf 
 sudo cp ~/rataim/910-rat.conf /etc/X11/xorg.conf.d/910-rat.conf
-touch ~/.xinitrc
-cp ~/rataim/rataim.conf 
+touch ~/.xbindkeysrc
+cp ~/rataim/rataim.conf ~/.xbindkeysrc 
 
