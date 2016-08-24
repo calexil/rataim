ae2614f0c07447bdb93bd0cb374b0098cfdb0d65
calexil <calexil@yahoo.com>
2016-08-23 20:28:31 -0400

    Install script fixing 1.1

--- install.sh
+++ install.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-gksudo mkdir /etc/X11/xorg.conf.d/;
-touch /etc/X11/xorg.conf.d/910-rat.conf;
-cp ~/$USER/rataim/910-rat.conf /etc/X11/xorg.conf.d/910-rat.conf;
+gksudo mkdir /etc/X11/xorg.conf.d/ 
+sudo touch /etc/X11/xorg.conf.d/910-rat.conf 
+sudo cp ~/rataim/910-rat.conf /etc/X11/xorg.conf.d/910-rat.conf
 
-print "success, you will now need to restart x or reboot"
-wait 10
-exit
