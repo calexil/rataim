329bb4b8df15f71682d00a57725b4b28c9dbdfb4
calexil <calexil@yahoo.com>
2016-08-28 18:30:07 -0400

    installer changes, added deps1.0

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@
 gksudo mkdir /etc/X11/xorg.conf.d/ 
 sudo touch /etc/X11/xorg.conf.d/910-rat.conf 
 sudo cp ~/rataim/910-rat.conf /etc/X11/xorg.conf.d/910-rat.conf
+sudo apt install xbindkeys
 touch ~/.xbindkeysrc
 cp ~/rataim/rataim.conf ~/.xbindkeysrc 
 
